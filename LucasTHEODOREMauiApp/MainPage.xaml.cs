5beaec0fce36207ced148257aaafa6f8c36a77ab
Anosy0 <lucas.theodore2004@gmail.com>
2024-03-08 14:38:12 +0100

    Add the bottom Menu

--- LucasTHEODOREMauiApp/MainPage.xaml.cs
+++ LucasTHEODOREMauiApp/MainPage.xaml.cs
@@ -7,9 +7,11 @@
         public MainPage()
         {
             InitializeComponent();
+            
+            
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        /*private void OnCounterClicked(object sender, EventArgs e)
         {
             count++;
 
@@ -19,7 +21,7 @@
                 CounterBtn.Text = $"Clicked {count} times";
 
             SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+        }*/
     }
 
 }
